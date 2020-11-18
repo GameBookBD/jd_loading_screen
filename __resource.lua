7aa609220187b5834e1be2a2fc295c19fd08fea2
Parag Ferdus <paragferdus@gmail.com>
2020-11-19 01:10:12 +0600

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -36,4 +36,4 @@ loadscreen 'index.html'
 loadscreen_manual_shutdown 'yes'
 client_script 'client.lua'
 
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
